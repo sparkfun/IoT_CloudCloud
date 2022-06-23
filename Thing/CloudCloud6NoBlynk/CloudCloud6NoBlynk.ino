41f13dd09cdd2f099c74fc08cbd3f9935c41179b
fourstix <fourstix@gmail.com>
2022-06-23 15:57:13 -0400

    Removed Blynk from ESP8266 Thing Dev code

--- Thing/CloudCloud6NoBlynk/CloudCloud6NoBlynk.ino
+++ Thing/CloudCloud6NoBlynk/CloudCloud6NoBlynk.ino
@@ -1,53 +1,38 @@
 /**
  * Based on Sparkfun CloudCloud
  * by Shawn Hymel @ SparkFun Electronics
+ * 
+ * Copyright (c) 2015-2022 by Gaston Willams
  *
  * September 17, 2015
+ * Initial code adapted from Sparkfun
+ * 
  * July 2, 2016
- *
- * Use Blynk app to set up some buttons and LEDs. See the pin
- * definitions below.
- *
  * Updated to handle multiple condition codes from OpenWeather API.
+ * 
+ * June 23, 2022
+ * Removed Blynk and blynk demo modes
+ * 
  */
 
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
 #include <WiFiUdp.h>
 #include <TimeLib.h>
 
 #include "States.h"
 #include "Secrets.h"
 
-#define DEBUG 0
-
-// Debug to Blynk Serial
-#if DEBUG
-#define BLYNK_PRINT Serial
-#endif
-
 // Constant for conditions
 #define MAX_CONDITIONS    3
-#define LCD_SIZE         20
 
 // Pin definitions
 #define LED_PIN           5
-#define WEATHER_BTN_PIN   V0
-#define WEATHER_LED_PIN   V1
-#define RGB_BTN_PIN       V2
-#define RGB_LED_PIN       V3
-#define DISCO_BTN_PIN     V4
-#define DISCO_LED_PIN     V5
-#define DEMO_BTN_PIN      V6
-#define DEMO_LED_PIN      V7
-#define ZERGBA_PIN        V8
-#define LCD_PIN           V9
 
 // NTP Constants
 #define NTP_PACKET_SIZE 48
 
-//Night Mode times - 10:30 pm to 5:00 am
-#define BEDTIME  81000
+//Sleep times - 10:00 pm to 5:00 am
+#define BEDTIME  79200
 #define WAKETIME 18000
 
 
@@ -72,38 +57,20 @@ const int ICON_SIZE = 4;    // bytes
 const int DT_SIZE = 12;     // bytes
 
 // Global variables
-WidgetLED weather_led(WEATHER_LED_PIN);
-WidgetLED rgb_led(RGB_LED_PIN);
-WidgetLED disco_led(DISCO_LED_PIN);
-WidgetLED demo_led(DEMO_LED_PIN);
-WidgetLCD lcd(LCD_PIN);
-CloudState cloud_state;
 WiFiClient client; // http client for API requests
 WiFiUDP Udp;       // udp client for NTP requests
 WeatherType current_weather[MAX_CONDITIONS];
-WeatherType demo_weather;
-unsigned long time_update;  // minute counter  
-unsigned long time_interval;  // 10 second interval counter
+unsigned long time_update = 0;  // minute counter  
+unsigned long time_interval = 0;  // 10 second interval counter
 bool force_update;
-uint8_t color_r;
-uint8_t color_g;
-uint8_t color_b;
 byte out_msg[OUT_BUF_MAX];
 int condition_count = 0;
 int condition_index = 0;
-char conditions[LCD_SIZE+1]; // 20 chars + null for string end
 boolean daybreak   = false;
 
 /*
- * Night mode will turn cloud off when the local time is between
- * bedtime and wake time.  
- */
-boolean night_mode = false;
-/*
- * Sleep mode indicates that the cloud is in night mode and the
- * local time is between bedtime and wake time.  We don't need
- * to query the weather during this time since the cloud is off.
- *
+ * Sleep mode indicates that the local time is between bedtime and wake time.  
+ * We don't need to query the weather during this time since the cloud is off.
  */
 boolean sleep_mode = false;
 
@@ -111,255 +78,63 @@ boolean sleep_mode = false;
  * Setup
  ***************************************************************/
 void setup() {
-  
-  uint8_t led = 0;
-
-  // Set up status LED
+  // Enable status LED
   pinMode(LED_PIN, OUTPUT);
-  
-  // Set up debug serial console
+  //Turn off, wifi turns on when it connects
+  digitalWrite(LED_PIN, LOW);
+    
+  // Set up serial communication
   Serial.begin(9600);
 
-  // Start Blynk and wait for connection
-  Blynk.begin(BLYNK_AUTH, WIFI_SSID, WIFI_PASS);
-  while ( Blynk.connect() == false ) {
-    led ^= 0x01;
-    digitalWrite(LED_PIN, led);
-    delay(200);
+  //Wifi ssid and password are defined in Secrets.h file
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  delay(500); //wait a bit to connect
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    //show disco mode (error) until wifi connected
+    sendDisco();
+    delay(500); //wait a bit and try again
   }
-  digitalWrite(LED_PIN, HIGH);
   
   // Set up time
   setSyncProvider(getNtpTime);
   setSyncInterval(1800);
-
   
   // Define initial state
-  cloud_state = CLOUD_WEATHER;
-  setLEDs();
   time_update = millis();
   time_interval = time_update;
   force_update == true;
-  color_r = 0;
-  color_g = 0;
-  color_b = 0;
-  lcd.clear();
-  demo_weather = WEATHER_UNKNOWN;
+ 
+  //set weather dark in case we are asleep
+  condition_count = 1;
+  current_weather[0] = WEATHER_DARK;
+  
+  //Turn cloud orange for 3 seconds
+  sendRGB(133, 58, 0);
+  delay(3000);
+
+  //Show each possible condition on the cloud
+  testWeather();
+
+  //then go dark until API update
+  sendRGB(0, 0, 0);
 } // setup
 
 /****************************************************************
  * Loop
  ***************************************************************/
 void loop() {
-  Blynk.run();
-  
-  // Perform LED functions
-  switch ( cloud_state ) {
-    
-    // Reflect weather data in clouds
-    case CLOUD_WEATHER:
-      doWeather();
-      break;
-      
-    case CLOUD_RGB:
-    case CLOUD_DISCO:
-    case CLOUD_DEMO:
-    default:
-      break;
-  } // switch
+  //After removing Blynk, just do weather all the time
+  doWeather();
+  delay(1000);
 } // loop
-
-/****************************************************************
- * Blynk Callbacks
- ***************************************************************/
-/*
- * grw - these Blynk controls are a bit overloaded because one 
- *     only gets so many controls on Blynk for free. If one
- *     is willing to pay more, the interface could be better.
- *     But I'm cheap, so it is what it is.
- */
- 
-// Callback when weather button is pushed
-// This button will toggle the night_mode off and on
-BLYNK_WRITE(WEATHER_BTN_PIN) {
-  if (param.asInt() == 1) {
-    // toggle night mode with each press
-    night_mode = !night_mode;
-    // if we turn off night_mode, turn off sleep_mode
-    if (!night_mode) {
-      sleep_mode = false;
-    }
-
-    // change state to weather if needed
-    if (cloud_state != CLOUD_WEATHER) {
-      cloud_state = CLOUD_WEATHER;
-    } // if !CLOUD_WEATHER
-    setLEDs();
-    force_update = true;
-
-  } // if param == 1
-} // BLYNK_WRITE(WEATHER_BTN_PIN)
-
-// Callback when RGB button is pushed
-BLYNK_WRITE(RGB_BTN_PIN) {
-  if ( (param.asInt() == 1) && (cloud_state != CLOUD_RGB) ) {
-    cloud_state = CLOUD_RGB;
-    setLEDs();
-    sendRGB();
-  } // if
-} // BLYNK_WRITE(RGB_BTN_PIN)
-
-// Callback when Disco button is pushed
-BLYNK_WRITE(DISCO_BTN_PIN) {
-  if ( (param.asInt() == 1) && (cloud_state != CLOUD_DISCO) ) {
-    cloud_state = CLOUD_DISCO;
-    setLEDs();
-    sendDisco();
-  } // if
-} // BLYNK_WRITE(DISCO_BTN_PIN)
-
-// Callback when the ZeRGBa changes
-BLYNK_WRITE(ZERGBA_PIN) {
-  color_r = param[0].asInt();
-  color_g = param[1].asInt();
-  color_b = param[2].asInt();
-  if ( cloud_state == CLOUD_RGB ) {
-    sendRGB();
-    lcd.clear();
-    lcd.print(0, 0, "RGB");
-    lcd.print(0, 1, "R");
-    lcd.print(1, 1, color_r);
-    lcd.print(5, 1, "G");
-    lcd.print(6, 1, color_g);
-    lcd.print(10, 1, "B");
-    lcd.print(11, 1, color_b);
-  } // if
-} // BLYNK_WRITE(ZERGBA_PIN)
-
-// Callback when demo button is pushed
-BLYNK_WRITE(DEMO_BTN_PIN) {
-  memset(out_msg, 0, 3);
-  if ( (param.asInt() == 1) ) {
-    cloud_state = CLOUD_DEMO;
-    setLEDs();
-    lcd.clear();
-    lcd.print(0, 0, "Demo");
-    switch ( demo_weather ) {
-      case WEATHER_BLUE_SKY:
-        demo_weather = WEATHER_NIGHT;
-        lcd.print(0, 1, "NIGHT");
-        out_msg[0] = COMM_NIGHT;
-        break;
-        
-      case WEATHER_NIGHT:
-        demo_weather = WEATHER_OVERCAST;
-        lcd.print(0, 1, "OVERCAST");
-        out_msg[0] = COMM_OVERCAST;
-        break;
-        
-      case WEATHER_OVERCAST:
-        demo_weather = WEATHER_GOLDEN;
-        lcd.print(0, 1, "GOLDEN");
-        out_msg[0] = COMM_GOLDEN;
-        break;
-        
-      case WEATHER_GOLDEN:
-        demo_weather = WEATHER_SNOW;
-        lcd.print(0, 1, "SNOW");
-        out_msg[0] = COMM_SNOW;
-        break;
-        
-      case WEATHER_SNOW:
-        demo_weather = WEATHER_LIGHTNING;
-        lcd.print(0, 1, "STORM");
-        out_msg[0] = COMM_LIGHTNING;
-        break;
-        
-      case WEATHER_LIGHTNING:
-        demo_weather = WEATHER_RAIN;
-        lcd.print(0, 1, "RAIN");
-        out_msg[0] = COMM_RAIN;
-        break;
-      case WEATHER_RAIN:
-        demo_weather = WEATHER_MIST;
-        lcd.print(0, 1, "MIST");
-        out_msg[0] = COMM_MIST;
-        break;        
-      case WEATHER_MIST: 
-        demo_weather = WEATHER_RAINBOW;
-        lcd.print(0, 1, "RAINBOW");
-        out_msg[0] = COMM_RAINBOW;
-        break;
-      case WEATHER_RAINBOW:
-        demo_weather = WEATHER_TWILIGHT;
-        lcd.print(0, 1, "TWILIGHT");
-        out_msg[0] = COMM_TWILIGHT;
-        break; 
-      case WEATHER_TWILIGHT:
-        demo_weather = WEATHER_DARK;
-        lcd.print(0, 1, "DARK");
-        out_msg[0] = COMM_DARK;
-        break;  
-      case WEATHER_DARK:  // falls through    
-      default:
-        demo_weather = WEATHER_BLUE_SKY;
-        lcd.print(0, 1, "BLUE SKY");
-        out_msg[0] = COMM_BLUE_SKY;
-        break;
-    } // switch
-    transmitMessage(out_msg, 3);
-  } // if
-} // BLYNK_WRITE(DEMO_BTN_PIN)
-
-// Set app LEDs
-void setLEDs() {
-  
-  // Turn them all off
-  weather_led.off();
-  rgb_led.off();
-  disco_led.off();
-  demo_led.off();
-  lcd.clear();
-  
-  // Only turn on the one we want
-  switch ( cloud_state ) {
-    case CLOUD_WEATHER:
-      weather_led.on();
-      if (night_mode) {
-        // indicate sleep mode
-        if (sleep_mode) {
-          lcd.print(0, 0, "(sleep)");
-        } else {
-        //grw - write state in different style to indicate night mode
-        lcd.print(0, 0, "(weather)");
-        } // if-else sleep_mode
-      } else {
-        lcd.print(0, 0, "WEATHER");
-      } // if-else night_mode
-      break;
-    case CLOUD_RGB:
-      rgb_led.on();
-      lcd.print(0, 0, "RGB");
-      break;
-    case CLOUD_DISCO:
-      disco_led.on();
-      lcd.print(0, 0, "DISCO");
-      break;
-    case CLOUD_DEMO:
-      demo_led.on();
-      lcd.print(0, 0, "DEMO");
-      break;      
-    default:
-      break;
-  }
-} // setLEDs
 
 /****************************************************************
  * Send Modes to Pro Mini
  ***************************************************************/
 
 // Send RGB code with red, green, blue values
-void sendRGB() {
+void sendRGB(uint8_t color_r, uint8_t color_g, uint8_t color_b) {
   out_msg[0] = COMM_RGB;
   out_msg[1] = color_r;
   out_msg[2] = color_g;
@@ -424,22 +199,27 @@ void sendWeather(int condition) {
         case WEATHER_LIGHTNING:
           out_msg[0] = COMM_LIGHTNING;
           break;
+          
         // Rain
         case WEATHER_RAIN:
           out_msg[0] = COMM_RAIN;
           break;  
+          
         //Fog, Mist or Haze
         case WEATHER_MIST:
           out_msg[0] = COMM_MIST;
           break;    
+          
         // Twilight
         case WEATHER_TWILIGHT:
           out_msg[0] = COMM_TWILIGHT;
           break;  
-        // Dark
+          
+        // Dark (Sleep time)
         case WEATHER_DARK:
           out_msg[0] = COMM_DARK;
-          break;          
+          break;      
+              
         // Unknown weather type        
         case WEATHER_UNKNOWN:
         default:
@@ -449,16 +229,26 @@ void sendWeather(int condition) {
       transmitMessage(out_msg, 4);
 } // sendWeather
 
+// send all weather conditions to the cloud one by one
+void testWeather() {
+  //step through each weather condition in enumeration
+  for (int condition = WEATHER_ERROR; condition <= WEATHER_UNKNOWN; condition++) {
+     out_msg[0] = condition;
+     transmitMessage(out_msg, 4);
+     //show the state long enough to view it
+     delay(5000);
+  } //for condition
+}// testWeather
+
 // Every 60 seconds, pull weather data and update LEDs
 // Updated logic to show error on lcd rather than cloud
 void doWeather() {  
   boolean result = false;
   // Only update every 60 seconds
-  if ( ((millis() - time_update) >= 60000) || force_update ) {
+  if ( force_update || ((millis() - time_update) >= 60000)) {
     force_update = false;
     time_update = millis();
     time_interval = time_update;
-    lcd.clear();
 
     //if we are asleep, check the time and update the sleep_mode
     if (sleep_mode) {
@@ -478,87 +268,12 @@ void doWeather() {
     
     // if we get new weather codes show them, 
     // otherwise continue scrolling old codes
-    if (result) {        
-      // We have new conditions, so clear string buffer and reset index
-      memset(conditions, 0, LCD_SIZE+1); // clear conditions buffer
-      
-      // Display weather on clouds
-      for(int i = 0; i < condition_count; i++ ) {
-        if (i > 0) {
-          strcat(conditions, ",");
-        } // if
-        switch ( current_weather[i] ) {      
-          // Cannot get weather data
-          case WEATHER_ERROR:
-            strcat(conditions, "Error");
-            break;
-          
-          // Clear skies (day)
-          case WEATHER_BLUE_SKY:
-            strcat(conditions, "Clear");
-            break;
-          
-          // Clear skies (night)
-          case WEATHER_NIGHT:
-            strcat(conditions, "Night");
-            break;
-          
-          // Lots of clouds
-          case WEATHER_OVERCAST:
-            strcat(conditions, "Clouds");
-            break;
-          
-          // Golden time is Sunrise or Sunset
-          case WEATHER_GOLDEN:
-            strcat(conditions, daybreak ? "Sunrise" : "Sunset");
-            break;
-          // Twilight is Dawn or dusk
-          case WEATHER_TWILIGHT:
-            strcat(conditions, daybreak ? "Dawn" : "Dusk");
-            break;
-            
-          // It's snowing!
-          case WEATHER_SNOW:
-            strcat(conditions, "Snow");
-            break;
-          
-          // Storm
-          case WEATHER_LIGHTNING:
-            strcat(conditions, "Storm");
-            break;
-          // Rain
-          case WEATHER_RAIN:
-            strcat(conditions, "Rain");
-            break;  
-          // Mist, fog or haze
-          case WEATHER_MIST:
-            strcat(conditions, "Mist");
-            break; 
-          // Dark (during night mode)
-          case WEATHER_DARK:
-            strcat(conditions, "Dark");
-            break;             
-          // Unknown weather type        
-          case WEATHER_UNKNOWN:
-          default:
-            strcat(conditions, "Other");
-            break;
-        } // switch      
-      } // for
-      if (night_mode) {
-         lcd.print(0, 0, "*Weather*");
-      } else {
-      lcd.print(0, 0, "Weather");
-      }
-      lcd.print(0, 1, conditions); 
+    if (result) {         
       //send the first weather condition
       condition_index = 0;
       sendWeather(current_weather[condition_index]);     
     } else {
-      // otherwise, show an error msg and next condition
-      lcd.print(0, 0, "API Error");
-      lcd.print(0, 1, conditions);
-      // show next condition from previoius request
+      // show next condition from previous request
       sendNextWeather();
     }  // if-else (result)
     // Update the cloud
@@ -666,7 +381,8 @@ boolean getWeather() {
   String cmd = "GET /data/2.5/weather?id=";
   // use city id for durham
   cmd += "4464368";
-  
+
+  //API ID string defined in Secrets.h file
   //You can get your own API id at https://home.openweathermap.org/users/sign_up 
   cmd += API_ID_STR;  
   
@@ -793,7 +509,7 @@ boolean getWeather() {
     // Set sleep mode flag and return
     sleep_mode = true;
     return true;
-  }  // if night_mode and dark
+  }  // if isSleepTime
   
   // Lookup what to display based on icon
   // http://openweathermap.org/weather-conditions
@@ -851,10 +567,11 @@ boolean getWeather() {
   return true;
 } // getWeather
 
-// See if we should go dark because of night mode
+// See if we should go dark because of night time
 boolean isSleepTime(unsigned long time_val) {  
-  return (night_mode && 
-          ((time_val > BEDTIME) || (time_val < WAKETIME)));
+//  return (night_mode && 
+//          ((time_val > BEDTIME) || (time_val < WAKETIME)));
+  return ((time_val > BEDTIME) || (time_val < WAKETIME));          
 }
 
 // Shift buffer to the left by 1 and append new char at end
--- Thing/CloudCloud6NoBlynk/Secrets.h
+++ Thing/CloudCloud6NoBlynk/Secrets.h
@@ -3,7 +3,6 @@
 
 #define WIFI_SSID  "Your_Wifi"
 #define WIFI_PASS  "Wifi_Password"
-#define BLYNK_AUTH "Your_Blink_Authorization"
 #define API_ID_STR "&appid=Your_API_ID_String"
 
 #endif // SECRETS_H
